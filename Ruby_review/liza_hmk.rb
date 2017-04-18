53484b332fbf0746d8769d031d7cd498221a829b
Matt <matt.fasano88@gmail>
2017-04-17 21:12:06 -0400

    liza_hmk

---
+++ Ruby_review/liza_hmk.rb
@@ -0,0 +1,25 @@
+# Explain the relationship between .new() and def initialize().
+# The def initialize() method runs when a .new() method is created!
+
+# Define class, subclass, instance, and self.
+# Class = an object blueprint
+# Sublcass = a class that inherits a parent class
+# Instance = an "instance" of a class or subclass
+# Self = an instances way of referring to itself
+
+# Properly define instance and class variables.
+# Instance Variable = is a varible only available for a class's instance
+# Class Variable = shared by all objects of a class
+
+# Properly define instance and class methods.
+# Instance method = Method you call on an instance
+# Class method = Method available for all objects of a class
+
+# Distinguish whether a piece of data is best suited to being stored in a local, instance, or class variable.
+# Data stored in a class variable is the best option because it can be used by multiple objects !
+
+# Describe the relationship of attr_ and "getter" and "setter" methods.
+# attr_getter "gets" the chosen attribute (i.e. color) and attr_getter sets the attribute!
+
+# Distinguish whether a piece of data is best suited to having its accessibility defined by attr_reader, attr_writer, attr_accessor or none of the above.
+# attr_accessor is the best choice for accessibility becuase it bypasses having to create instance methods, or using attr_reader, attr_writer to create attributes! This is a great shortcut and save the programmer time !
